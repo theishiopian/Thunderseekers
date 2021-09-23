742f448d12926ae32dcd3154967ee2dcec28d7ee
theishiopian <theishiopian@gmail.com>
2021-09-23 12:33:39 -0700

    NetworkedTransform implemented

--- Assets/Scripts/NetworkedTransform.cs
+++ Assets/Scripts/NetworkedTransform.cs
@@ -1,9 +1,27 @@
 using MLAPI;
+using MLAPI.NetworkVariable;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// This component stores its transform data from the server/host and syncs it on the client
+/// </summary>
 public class NetworkedTransform : NetworkBehaviour
 {
-    
+    NetworkVariable<Vector3> syncPos = new NetworkVariable<Vector3>();
+    NetworkVariable<Quaternion> syncRot = new NetworkVariable<Quaternion>();
+
+    private void FixedUpdate()
+    {
+        if(IsClient)
+        {
+            transform.position = syncPos.Value;
+            transform.rotation = syncRot.Value;
+        }
+        else
+        {
+            syncPos.Value = transform.position;
+            syncRot.Value = transform.rotation;
+        }
+    }
 }
