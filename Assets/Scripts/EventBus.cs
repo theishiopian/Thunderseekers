9945f09e3b3c3e26561cc0063ecf9a746bb6a17e
theishiopian <theishiopian@gmail.com>
2021-09-25 01:42:10 -0700

    Event bus

--- Assets/Scripts/EventBus.cs
+++ Assets/Scripts/EventBus.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventBus : MonoBehaviour
+public delegate void GameEvent(params object[] p);
+/// <summary>
+/// This class stores events and handles access to them
+/// </summary>
+public class EventBus
 {
-    // Start is called before the first frame update
-    void Start()
+    private static Dictionary<string, GameEvent> events = new Dictionary<string, GameEvent>();
+
+    public static void Invoke(string eventID, params object[] p)
     {
-        
+        events[eventID]?.Invoke(p);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void RegisterListner(string eventID, GameEvent @event)
     {
-        
+        if(events.ContainsKey(eventID))
+        {
+            events[eventID] += @event;
+        }
+        else
+        {
+            Debug.Log("Registering new event with ID: " + eventID);
+            events.Add(eventID, new GameEvent(@event));
+        }
     }
 }
